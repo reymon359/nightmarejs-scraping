acb119871cdf8c3bd0fbef1c49c0abbbc8b6fb14
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-09-24 07:30:44 +0200

    modulate and pagination

--- scraping using vanilla javascript/index.js
+++ scraping using vanilla javascript/index.js
@@ -1,49 +1,13 @@
-const Nightmare = require('nightmare')
-const nightmare = Nightmare({
-    show: true
-});
+const ycombinator = require('./ycombinator');
 
 (async() => {
-    await nightmare.goto('https://news.ycombinator.com/news');
+    await ycombinator.initialize();
 
-    let articles = await nightmare.evaluate(() => {
-
-        let tableRows = document.querySelectorAll('table[class="itemlist"] > tbody > tr');
-
-
-        let articles = [];
-
-        for (let row of tableRows) {
-
-            if (row.getAttribute('class') == 'spacer') continue;
-
-            if (row.getAttribute('class') == 'athing') {
-
-                let title = row.querySelector('td[class="title"] > a').innerText;
-                let url = row.querySelector('td[class="title"] > a').getAttribute('href');
-                let source = row.querySelector('span[class="sitebit comhead"] > a') ? row.querySelector('span[class="sitebit comhead"] > a').innerText : false;
-
-                let secondRow = row.nextSibling;
-
-                let points = secondRow.querySelector('span[class="score"]') ? secondRow.querySelector('span[class="score"]').innerText : false;
-                let author = secondRow.querySelector('a[class="hnuser"]') ? secondRow.querySelector('a[class="hnuser"]').innerText : false;
-                let date = secondRow.querySelector('span[class="age"]').innerText;
-                let comments = secondRow.querySelectorAll('a')[3] ? secondRow.querySelectorAll('a')[3].innerText : false;
-
-                articles.push({ title, url, source, points, author, date, comments });
-
-            }
-
-        }
-
-        return articles;
-
-    });
+    let articles = await ycombinator.getArticles(65);
 
     console.log(articles);
 
     debugger;
-
 
 
 })();
