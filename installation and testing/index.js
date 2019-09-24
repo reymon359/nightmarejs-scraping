0de5c629fbfe5d6868ca69c46b1f81f923ed2f21
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-09-24 07:11:31 +0200

    using async

--- installation and testing/index.js
+++ installation and testing/index.js
@@ -1,14 +1,19 @@
 const Nightmare = require('nightmare')
-const nightmare = Nightmare({ show: true }) // { headless: false }
+const nightmare = Nightmare({ show: true }); // { headless: false }
 
-nightmare
-    .goto('https://duckduckgo.com')
-    .type('#search_form_input_homepage', 'github nightmare')
-    .click('#search_button_homepage')
-    .wait('#r1-0 a.result__a')
-    .evaluate(() => document.querySelector('#r1-0 a.result__a').href)
-    .end()
-    .then(console.log)
-    .catch(error => {
-        console.error('Search failed:', error)
-    })
+(async() => {
+
+    try {
+        await nightmare.goto('https://duckduckgo.com');
+        await nightmare.type('#search_form_input_homepage', 'github nightmare');
+        await nightmare.click('#search_button_homepage');
+        await nightmare.wait('#r1-0 a.result__a');
+        let link = await nightmare.evaluate(() => document.querySelector('#r1-0 a.result__a').href);
+        await nightmare.end();
+
+        console.log(link);
+    } catch (error) {
+        console.log(`Something happened: ${error}`);
+    }
+
+})();
